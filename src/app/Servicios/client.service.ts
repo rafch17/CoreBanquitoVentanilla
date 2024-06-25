126be81a4f857657054dcd4eb0118c5dc7d0d59b
Rafch17 <randyni46@gmail.com>
2024-06-25 14:37:22 -0500

    preuba https

--- src/app/Servicios/client.service.ts
+++ src/app/Servicios/client.service.ts
@@ -7,7 +7,7 @@ import { Observable } from 'rxjs';
 })
 export class ClientService {
 
-  private searchClientApi = 'http://corebanquito-client.us-east-1.elasticbeanstalk.com/client/'; // URL del endpoint
+  private searchClientApi = 'https://corebanquito-client.us-east-1.elasticbeanstalk.com/client/'; // URL del endpoint
   constructor(private http: HttpClient) { }
   searchAcount(clientNumber:string):Observable<any>{
     return this.http.get<any>(this.searchClientApi + clientNumber);
