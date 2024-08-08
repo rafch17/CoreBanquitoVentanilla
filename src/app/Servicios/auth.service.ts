53186d201e08617e58f9016205e718bb846cc2d1
Rafch17 <randyni46@gmail.com>
2024-08-07 20:07:42 -0500

    455

--- src/app/Servicios/auth.service.ts
+++ src/app/Servicios/auth.service.ts
@@ -9,7 +9,7 @@ export class AuthService {
 
 //  private loginUrl = 'http://localhost:8082/login'; // URL del endpoint
 
-  private loginUrl = 'http://core-cobros-alb-538320160.us-east-1.elb.amazonaws.com/api/v1/auth/login'; // URL del endpoint
+  private loginUrl = 'https://08zhnuj863.execute-api.us-east-1.amazonaws.com/banquito/api/v1/auth/login'; // URL del endpoint
 
   constructor(private http: HttpClient) { }
   login(userName: string, password: string): Observable<any> {
--- src/app/Servicios/client.service.ts
+++ src/app/Servicios/client.service.ts
@@ -7,7 +7,7 @@ import { Observable } from 'rxjs';
 })
 export class ClientService {
 
-  private searchClientApi = 'http://core-cobros-alb-538320160.us-east-1.elb.amazonaws.com/client-microservice/api/v1/clients/'; // URL del endpoint
+  private searchClientApi = 'https://08zhnuj863.execute-api.us-east-1.amazonaws.com/banquito/client-microservice/api/v1/clients/'; // URL del endpoint
   constructor(private http: HttpClient) { }
   searchAcount(clientNumber:string):Observable<any>{
     return this.http.get<any>(this.searchClientApi + clientNumber);
